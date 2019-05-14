d6bff1131e679ad838f0ddb808c782eb79f83f20
mutichung <mtchung037@gmail.com>
2019-05-14 16:46:52 +0800

    test class vec3D

--- Cpp_test/Con_Distructors.cpp
+++ Cpp_test/Con_Distructors.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 using namespace std;
 
-class A{
-    public:
-        A() {
-            cout << "A\n";
-        }
-
-        ~A(){
-            cout << "a\n";
-        }
-};
-
-class B{
+class vec3D{
     private:
-        A a;
+        double* addr;
     public:
-        B() {
-            cout << "B\n";
-        }
+        vec3D(){
+            addr = new double[3];
+            for(int i = 1; i <= 3; i++) addr[i] = 0;
+        };
+        void print();
 
-        ~B() {
-            cout << "b\n";
-        }
 };
 
-int main(){
-    B b;
+int main()
+{
+    vec3D v1;
+    v1.print();
     return 0;
 }
+
+void vec3D::print(){
+    cout << "\n(";
+    for(int i = 1; i <= 3; i++){
+        cout << " " << addr[i-1] << ",";
+    }
+    cout << ")\n";
+};
