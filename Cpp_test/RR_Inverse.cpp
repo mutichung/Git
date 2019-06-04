a3462e6805e817b635be23818b85430321545656
mutichung <mtchung037@gmail.com>
2019-06-04 17:45:47 +0800

    new RR_Inverse.cpp

---
+++ Cpp_test/RR_Inverse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//#include <Eigen/Dense>
+#include <math.h>
+using namespace std;
+//using namespace Eigen;
+
+int main(){
+    double l1 = 10, l2 = 8;
+    double t1 = 0, t2 = 0;
+    double px, py;
+    
+}
