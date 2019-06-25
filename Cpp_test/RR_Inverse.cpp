97d2ffef3934fa4f5626eadcda1165da8f3c96c6
mutichung <mtchung037@gmail.com>
2019-06-25 15:53:39 +0800

    class DHTable

--- Cpp_test/RR_Inverse.cpp
+++ Cpp_test/RR_Inverse.cpp
@@ -4,14 +4,48 @@
 using namespace std;
 using namespace Eigen;
 
+Matrix4d TfMat(MatrixX4d dh_table, int row);
+
 int main(){
     
     double l1 = 10, l2 = 8;
     double t1 = 0, t2 = 0;
     double px, py;
-    
-    MatrixX4d DH = MatrixX4d::Zero(2, 4);
-    
-    cout << endl << DH << "\n\n";
+    MatrixX4d dh = MatrixX4d::Zero(2, 4);
+    Matrix4d temp = TfMat(dh, 0);    
 
+
+    cout << endl << temp << "\n\n";
+
+}
+
+class DHTable{
+    private:
+        unsigned int number_of_nodes;
+        double** table;
+    public:
+        DHTable(unsigned int number_of_nodes);
+        
+};
+
+DHTable::DHTable(unsigned int number_of_nodes){
+    this->number_of_nodes = number_of_nodes;
+    table = new double*[this->number_of_nodes];
+    for(int i = 0; i < this->number_of_nodes; i++)  table[i] = new double[4];
+    for(int i = 0; i < this->number_of_nodes; i++){
+        for(int j = 0; j < 4, j++)  table[i][j] = 0;
+    }
+};
+
+
+
+Matrix4d TfMat(MatrixX4d dh_table, int row){
+    Matrix4d temp = Matrix4d::Identity(4, 4);
+    temp(0, 0) = cos(dh_table(row, 3));
+    temp(0, 1) = -sin(dh_table(row, 3));
+    temp(0, 3) = dh_table(row, 1);
+    temp(1, 0) = sin(dh_table(row, 3))*cos(dh_table(row, 0));
+    temp(1, 1) = cos(dh_table(row, 3))*sin(dh_table(row, 1));
+    temp(1, 2) = -sin(0);
+    return temp;
 }
