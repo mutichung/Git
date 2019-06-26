9e65be3ced4f735424e3eb8e469fdade96ee7b8f
mutichung <mtchung037@gmail.com>
2019-06-26 21:52:56 +0800

    modified TfMat

--- Cpp_test/DHTable.h
+++ Cpp_test/DHTable.h
@@ -14,9 +14,11 @@ class DHTable{
     public:
         DHTable(unsigned int number_of_nodes);
         ~DHTable();
+        MatrixX4d &set_table();
         double alpha(unsigned int i), a(unsigned int i), d(unsigned int i), theta(unsigned int i);
         VectorXd alpha(), a(), d(), theta();
         double &set_alpha(unsigned int i), &set_a(unsigned int i), &set_d(unsigned int i), &set_theta(unsigned int i);
+        double &set_alpha(unsigned int start_row, unsigned int end_row);
         void print_table() const;
         Matrix4d TfMat();
         Matrix4d TfMat(unsigned int cur_frame, unsigned int ref_frame);
@@ -30,24 +32,25 @@ DHTable::DHTable(unsigned int number_of_nodes){
 DHTable::~DHTable(){
     //TODO: remove memory allocate
 };
+MatrixX4d &DHTable::set_table(){ return table; };
 
 double DHTable::alpha(unsigned int i){  return table(i-1, 0); };
 double DHTable::a(unsigned int i){  return table(i-1, 1); };
 double DHTable::d(unsigned int i){  return table(i-1, 2); };
 double DHTable::theta(unsigned int i){  return table(i-1, 3); };
 
-VectorXd DHTable::alpha(){   return table.col(0);    };
-VectorXd DHTable::a(){   return table.col(1);    };
-VectorXd DHTable::d(){   return table.col(2);    };
-VectorXd DHTable::theta(){   return table.col(3);    };
+VectorXd DHTable::alpha(){ return table.col(0); };
+VectorXd DHTable::a(){ return table.col(1); };
+VectorXd DHTable::d(){ return table.col(2); };
+VectorXd DHTable::theta(){ return table.col(3); };
 
-double& DHTable::set_alpha(unsigned int i){ return table(i-1, 0); };
-double& DHTable::set_a(unsigned int i){ return table(i-1, 1); };
-double& DHTable::set_d(unsigned int i){ return table(i-1, 2); };
-double& DHTable::set_theta(unsigned int i){ return table(i-1, 3); };
+double &DHTable::set_alpha(unsigned int i){ return table(i-1, 0); };
+double &DHTable::set_a(unsigned int i){ return table(i-1, 1); };
+double &DHTable::set_d(unsigned int i){ return table(i-1, 2); };
+double &DHTable::set_theta(unsigned int i){ return table(i-1, 3); };
 
 void DHTable::print_table() const{
-    cout << "\n===DH Table===\n\n" << table << "\n\n==============\n";
+    cout << "\n===DH Table===\n\n" << table << "\n\n==============\n\n";
 };
 
 Matrix4d DHTable::TfMat(){
@@ -57,7 +60,7 @@ Matrix4d DHTable::TfMat(){
                 sin(theta(i))*cos(alpha(i)), cos(theta(i))*cos(alpha(i)), -sin(alpha(i)), -d(i)*sin(alpha(i)),
                 sin(theta(i))*sin(alpha(i)), cos(theta(i))*sin(alpha(i)), cos(alpha(i)), d(i)*cos(alpha(i)),
                 0, 0, 0, 1;
-        out *= temp;
+        out = temp*out;
     }
     return out;
 }
--- Cpp_test/RR_Inverse.cpp
+++ Cpp_test/RR_Inverse.cpp
@@ -11,9 +11,18 @@ int main(){
     double px, py;
 
     DHTable temp(3);
+    temp.set_table() << 0, 0, 0, 0,
+                        0, l1, 0, M_PI_2,
+                        0, l2, 0, 0;
+    temp.print_table();
+    cout << temp.TfMat() << "\n\n";
+    cout << temp.a() << "\n";
+    
+    /*
     temp.set_a(2) = l1;
     temp.set_a(3) = l2;
     temp.set_theta(3) = M_PI_2;
     temp.print_table();
     cout << endl << temp.TfMat(0, 3) << "\n\n";
+    */
 };
