b00534f73f26395a5366e29b18c2a5c579675fd2
mutichung <mtchung037@gmail.com>
2019-06-27 17:00:10 +0800

    IK_ana debug undone

--- Cpp_test/DHTable.h
+++ Cpp_test/DHTable.h
@@ -9,16 +9,17 @@ using namespace Eigen;
 
 class DHTable{
     private:
-        unsigned int number_of_nodes;
+        unsigned int number_of_rows;
         MatrixX4d table;
     public:
         DHTable(unsigned int number_of_nodes);
         ~DHTable();
         MatrixX4d &set_table();
+        unsigned int get_number_of_rows();
         double alpha(unsigned int i), a(unsigned int i), d(unsigned int i), theta(unsigned int i);
         VectorXd alpha(), a(), d(), theta();
         double &set_alpha(unsigned int i), &set_a(unsigned int i), &set_d(unsigned int i), &set_theta(unsigned int i);
-        double &set_alpha(unsigned int start_row, unsigned int end_row);
+        Vector3d &set_theta();
         void print_table() const;
         Matrix4d TfMat();
         Matrix4d TfMat(unsigned int cur_frame, unsigned int ref_frame);
@@ -26,8 +27,8 @@ class DHTable{
 #endif
 
 DHTable::DHTable(unsigned int number_of_nodes){
-    this->number_of_nodes = number_of_nodes;
-    table = MatrixX4d::Zero(this->number_of_nodes, 4);
+    number_of_rows = number_of_nodes+1;
+    table = MatrixX4d::Zero(this->number_of_rows, 4);
 };
 
 DHTable::~DHTable(){
@@ -35,6 +36,8 @@ DHTable::~DHTable(){
 };
 
 MatrixX4d &DHTable::set_table(){ return table; };
+
+unsigned int DHTable::get_number_of_rows(){ return number_of_rows; };
 
 double DHTable::alpha(unsigned int i){  return table(i-1, 0); };
 double DHTable::a(unsigned int i){  return table(i-1, 1); };
@@ -51,13 +54,16 @@ double &DHTable::set_a(unsigned int i){ return table(i-1, 1); };
 double &DHTable::set_d(unsigned int i){ return table(i-1, 2); };
 double &DHTable::set_theta(unsigned int i){ return table(i-1, 3); };
 
+//Vector3d &DHTable::set_theta(){ return table.block<this->number_of_rows, 1>(0, 3); };
+
+
 void DHTable::print_table() const{
     cout << "\n===DH Table===\n\n" << table << "\n\n==============\n\n";
 };
 
 Matrix4d DHTable::TfMat(){
     Matrix4d temp, out = Matrix4d::Identity(4, 4);
-    for(int i = number_of_nodes; i > 0; i--){
+    for(int i = number_of_rows; i > 0; i--){
         temp << cos(theta(i)), -sin(theta(i)), 0, a(i),
                 sin(theta(i))*cos(alpha(i)), cos(theta(i))*cos(alpha(i)), -sin(alpha(i)), -d(i)*sin(alpha(i)),
                 sin(theta(i))*sin(alpha(i)), cos(theta(i))*sin(alpha(i)), cos(alpha(i)), d(i)*cos(alpha(i)),
--- Cpp_test/RR_Inverse.cpp
+++ Cpp_test/RR_Inverse.cpp
@@ -4,19 +4,71 @@
 #include "DHTable.h"
 using namespace std;
 
+VectorXd IK_ana(Vector3d desired_pos, DHTable dh);
+double rad2deg(double input);
+double deg2rad(double inpud);
+VectorXd rad2deg(VectorXd input);
+
 int main(){
     
     double l1 = 10, l2 = 8;
     double t1 = 0, t2 = 0;
     double px, py;
+    double t = 50;
 
-    DHTable temp(3);
+    DHTable temp(2);
     temp.set_table() << 0, 0, 0, 0,
-                        0, l1, 0, M_PI_2,
+                        0, l1, 0, 0,
                         0, l2, 0, 0;
     temp.print_table();
-    cout << temp.TfMat() << "\n\n";
-    cout << temp.a() << "\n";
-    
+    Vector3d destination;
+    destination << -10, -8, 0;
 
+    cout << endl << destination << endl;
+
+    VectorXd sol(2);
+    sol = IK_ana(destination, temp);
+    cout << endl << rad2deg(sol) << endl;
+
+    temp.set_table().block<2,1>(0,3) = sol;
+    cout << endl << temp.TfMat() << endl;
+
+    /* 
+    cout << temp.TfMat() << "\n";
+    cout << temp.TfMat().block<2,1>(0,3) << "\n\n";
+    */
 };
+
+
+VectorXd IK_ana(Vector3d desired_pos, DHTable dh){
+    VectorXd temp(dh.get_number_of_rows()-1);  //theta1, theta2
+    double x = desired_pos(0);
+    double y = desired_pos(1);
+    double l1 = dh.a(2);
+    double l2 = dh.a(3);
+
+    //solve theta2
+    double t2 = acos((pow(x,2) + pow(y,2) - pow(l1,2) - pow(l2,2)) / (2*l1*l2));
+    
+    //solve theta1
+    double a, b, c;
+    a = x + l1 + l2*cos(t2);
+    b = 2*l2*sin(t2);
+    c = x - l1 - l2*cos(t2);
+    double t1 = 2*atan((-b + sqrt(pow(b,2) - 4*a*c)) / (2*a));
+    temp << t1, t2;
+
+    return temp;
+}
+
+double deg2rad(double input){ return input / 180 * M_PI; }
+double rad2deg(double input){
+    double temp = input / M_PI * 180;
+    return temp;
+}
+
+VectorXd rad2deg(VectorXd input){
+    VectorXd temp(input.size());
+    for(int i = 0; i < input.size(); i++)   temp(i) = input(i) / M_PI * 180;
+    return temp;
+}
