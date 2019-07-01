c0c0c34327502d168ba25f6a4f60e2b2823d6d88
mutichung <mtchung037@gmail.com>
2019-07-01 17:39:28 +0800

    Hello

--- Cpp_test/Hello.cpp
+++ Cpp_test/Hello.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
-    int a = 1, b = 2;
-    cout << a << " + " << b << " = " << a+b;
-    cout << "\nHello World!\n";
+    int bad = 0;
+    for(int i = 0; i <= 100; i++){
+        float f = sqrt(i);
+        cout << f << " " << f * f << " ";
+
+        if(f * f != i){
+            cout << "!!!";
+            bad++;
+        }
+    cout << "\n";
+    }
+    cout << "bad precision: " << bad << "\n\n";
     return 0;
 }
